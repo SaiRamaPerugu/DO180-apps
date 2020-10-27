bca93af3ceee2329e7a131ba4371b2d81c1c4910
Student User <student@workstation.lab.example.com>
2020-10-27 16:25:46 -0400

    fix

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . A chang is coming in. "\n"; 
+print "Hello, World! php version is " . PHP_VERSION . " a change is coming in" . "\n"; 
 ?>
